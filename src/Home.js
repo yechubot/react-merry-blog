82824e63377e24e86f986d4dc263549931697724
yechu <yechukim217@gmail.com>
2021-01-11 00:13:26 +0900

    json added

--- src/Home.js
+++ src/Home.js
@@ -2,28 +2,27 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogLists";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'welcome to my blog', body: 'this is my ...', author: 'yechu', id: 1 },
-        { title: 'how to make a snakbar on android?', body: 'this is my ...', author: 'flutter', id: 2 },
-        { title: 'welcome people', body: 'this is my ...', author: 'mario', id: 3 }]);
-
-        const [name,setName] = useState('mario');
+    const [blogs, setBlogs] = useState(null);
+       
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !==id); // match -> delete
         setBlogs(newBlogs);
     }
 
     useEffect(()=> {
-        console.log('use effect ran');
-        console.log(name);
-    },name);//name 변할때만 호출됨 
+        fetch('http://localhost:8000/blogs')
+        .then(res=> {
+            return res.json()
+        })
+        .then((data)=> {
+            setBlogs(data);
+        })
+    },[]);
+
     return (
         <div className="home">
-         <BlogList title="All Blogs " blogs ={blogs} handleDelete={handleDelete}/>
-         <BlogList title="Mario Blogs" blogs ={blogs.filter((blog)=> blog.author==='mario')}
-         handleDelete={handleDelete}/>
-         <button onClick={()=> setName('yechu')}>change name</button>
-         <p>{name}</p>
+        {blogs && <BlogList title="All Blogs " blogs ={blogs} handleDelete={handleDelete}/>}
+        
             <div className="go-github">
                 <button href="/">Go to yeeun's github </button>
             </div>
